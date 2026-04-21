8a534fb87824e4caf702c84d7aeb850d3f5ff3b6
Junmx01 <160344883+Junmx01@users.noreply.github.com>
2026-04-21 09:59:42 +0200

    Update version number to 1.0.1

--- cezanne-export.user.js
+++ cezanne-export.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cezanne - Exportar Maestro de empleados
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Exportador con autorización por código
 // @match        *://*/CezanneHR/*
 // @grant        GM_addStyle
